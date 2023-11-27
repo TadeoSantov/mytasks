9f909acd8b41b083b5791d748885b325313c7332
TadeoSantov <tadeosantovena@gmail.com>
2023-11-26 20:45:00 -0600

    .

---
+++ app/completed/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React from 'react'
+
+function completed() {
+  return <div>completed</div>;
+}
+
+export default completed
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Sidebar from './Components/Sidebar/Sidebar'
+import GlobalStyleProvider from './providers/GlobalStyleProvider'
+import ContextProvider from './providers/ContextProvider'
+import { ClerkProvider, auth } from '@clerk/nextjs'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,9 +18,27 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+
+  const { userId } = auth()
+
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <head>
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+            integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+            crossOrigin="anonymous"
+            referrerPolicy="no-referrer" />
+        </head>
+        <body className={inter.className}>
+          <ContextProvider>
+            <GlobalStyleProvider>
+              {userId && <Sidebar />}
+              <div className="w-full">{children}</div>
+            </GlobalStyleProvider>
+          </ContextProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
